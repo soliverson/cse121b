3f768b0e6f395c6fb832e44f9fdcdd68b1e8fd34
soliverson <stacy.oliverson@gmail.com>
2023-09-22 11:10:38 -0600

    Changed to different image.

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 let fullName = 'Stacy Oliverson';
 let currentYear = new Date().getFullYear();
-let profilePicture = 'images/StacyImage.jpg';
+let profilePicture = 'https://picsum.photos/536/354';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
