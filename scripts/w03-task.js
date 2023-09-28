1675d47cdefc629e465350488d438bd4636ef7db
soliverson <stacy.oliverson@gmail.com>
2023-09-27 18:27:52 -0600

    Update

---
+++ scripts/w03-task.js
@@ -0,0 +1,40 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add(num1, num2) 
+    {return number1 + number2;
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+    document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+}
+    
+/* Function Expression - Subtract Numbers */
+function subtract(num1, num2) 
+    {return number1 - number2;
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+    document.querySelector('#subtractNumbers').subtractEventListener('click', subtractNumbers);
+}
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
