05687b3f16cd990911b8d63f0ae694f76664566a
soliverson <stacy.oliverson@gmail.com>
2023-10-18 20:51:59 -0600

    Project Completed

--- scripts/project.js
+++ scripts/project.js
@@ -1,23 +1,19 @@
 let goatPicture = 'images/goats.jpg';
 
-function calculateFutureDate() {
+function calculateDate() {
+    // Get the user's input date
     const inputDateElement = document.getElementById("inputDate");
+    const inputDate = new Date(inputDateElement.value);
+
+    // Calculate the date that is 150 days in the future
+    const futureDate = new Date(inputDate);
+    futureDate.setDate(futureDate.getDate() + 150);
+
+    // Format the date as "month date year"
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = futureDate.toLocaleDateString(undefined, options);
+
+    // Display the result
     const resultElement = document.getElementById("result");
-
-    // Get the selected date from the input field
-    const selectedDate = new Date(inputDateElement.value);
-
-    if (isNaN(selectedDate)) {
-        resultElement.textContent = "Invalid date. Please enter a valid date.";
-        return;
-    }
-
-    // Calculate the future date by adding 150 days
-    const futureDate = new Date(selectedDate);
-    futureDate.setDate(selectedDate.getDate() + 150);
-
-    // Format the future date as "YYYY-MM-DD"
-    const formattedFutureDate = futureDate.toISOString().split('T')[0];
-
-    resultElement.textContent = `Your goat will have babies around: ${formattedFutureDate}`;
+    resultElement.textContent = `Your Goat will have babies around: ${formattedDate}`;
 }
